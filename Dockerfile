01d89738eabf9d20e2effc8c8aa9522b995ec76c
vbluuiza <luizavieirabelo3008@gmail.com>
2025-06-12 18:15:10 -0300

    ✨feat: ajuste para salvar colunas de horário como TIME no Postgres
    - Altera transformação das colunas measurement_datetime, sunrise_datetime e sunset_datetime para tipo time
    - Adiciona dtype_map no to_sql para garantir persistência correta no banco
    - Atualiza instruções e comentários para refletir o novo comportamento
    - Garante compatibilidade com schema do banco após alteração manual das colunas para TIME

--- Dockerfile
+++ Dockerfile
@@ -3,19 +3,22 @@ FROM python:3.11-slim
 
 # Instale dependências do sistema
 RUN apt-get update && apt-get install -y \
+    # build-essential e libpq-dev são necessários para compilar o psycopg2 (driver do Postgres)
     build-essential \
     libpq-dev \
+    # Limpa o cache do apt para manter a imagem final menor
     && rm -rf /var/lib/apt/lists/*
 
 # Defina o diretório de trabalho
 WORKDIR /app
 
-# Copie os arquivos do projeto
-COPY . .
-
-# Instale as dependências do Python
+# Otimização de Cache: Copiar e Instalar Dependências do Python
+COPY requirements.txt .
 RUN pip install --upgrade pip
-RUN pip install -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+# 5. Copiar o Código da Aplicação
+COPY . .
 
 # Comando para rodar o pipeline
 CMD ["python", "main.py"]
--- src/load/load_data.py
+++ src/load/load_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sqlalchemy import create_engine
+from sqlalchemy.types import Time
 from dotenv import load_dotenv
 import os
 import glob
@@ -32,19 +33,21 @@ def load_data(folder_path):
     # Lê o arquivo CSV em um DataFrame do pandas
     print('Lendo dados do arquivo CSV... √')
     df = pd.read_csv(path)
+    print(df.head())
     
     # Formantando o tipo das colunas no date_columns
     for col in date_columns:
         if col in df.columns:
-            df[col] = pd.to_datetime(df[col], format='%H:%M:%S', errors='coerce')
-    
+            df[col] = pd.to_datetime(df[col], format='%H:%M:%S', errors='coerce').dt.time
+            
     # Cria a engine de conexão com o banco de dados
-    print('Conectando ao banco de dados... √')
+    print(f'Conectando ao banco de dados...{database_url} √')
     engine = create_engine(database_url)
     
     
     # Salva os dados no banco de dados na tabela especificada
     print('Enviando dados para a tabela "recife_weather_records" √')
+    dtype_map = {col: Time() for col in date_columns}
     df.to_sql("recife_weather_records", con=engine, if_exists="append", index=False)
     
     print("\n✅ Dados salvos com sucesso no Postgres")
